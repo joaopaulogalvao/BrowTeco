d361076615a09709772a10c3071a2344a095cedf
Joao Alves <joaopaulogalvao1@gmail.com>
2016-04-02 11:14:25 -0300

    Change .gitignore to ignore certificates. Download new certificates. Redo all process from objc.io. Build the project before sending to GitHub and Travis.

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -11,7 +11,7 @@ fi
 # Thanks @djacobs https://gist.github.com/djacobs/2411095
 # Thanks @johanneswuerbach https://gist.github.com/johanneswuerbach/5559514
 
-PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/iOS_Team_Provisioning_Profile_.mobileprovision.enc"
+PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
 echo "***************************"
